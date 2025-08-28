b775920952945f30a231f3106e4fd7589545b6b3
Arbaj Shaikh <sarbaj637@gmail.com>
2025-08-28 16:35:40 +0530

    Arbaj Assignment  Answer

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,36 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  let [txt, setTxt] = useState("");
+
+  function handleKeyUp(event) {
+    if (event.target.value) {
+      setTxt(`Hello, ${event.target.value}!`);
+    } else {
+      setTxt("");
+    }
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+      {/* Do not remove the main div */}
+      <br></br>
+      <p>Enter Your Name: </p>
+      <br></br>
+      <div>
+        <textarea
+          type="text"
+          placeholder="Enter Your Name"
+          onKeyUp={handleKeyUp}
+        >
+          {" "}
+        </textarea>
+      </div>
+      <br></br>
 
-export default App
+      <p>{txt}</p>
+    </div>
+  );
+};
+
+export default App;
